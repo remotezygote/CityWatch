1aac277a3528527fdd860783d048edc632922a0a
John Bragg <remotezygote@gmail.com>
2013-02-08 15:00:58 -0800

    get nginx stub status stats if available + tally unicorn memory usage

--- lib/city_watch/watchmen/nginx.rb
+++ lib/city_watch/watchmen/nginx.rb
@@ -12,7 +12,22 @@ class Nginx
 			end
 			acc
 		end
-		out.merge({:num_masters => out[:masters].count, :num_workers => out[:workers].count, :summary => [:num_masters, :num_workers]})
+		
+		require 'net/http'
+		require 'net/https'
+		uri = URI.parse("http://localhost/nginx_status")
+		http = Net::HTTP.new(uri.host, uri.port)
+		req = Net::HTTP::Get.new(uri.path)
+		resp = http.request(req)
+		case resp
+			when Net::HTTPSuccess
+				active, garbage, nums, open = resp.read_body.split("\n")
+				accepts, handled, requests = nums.split
+				garbage, reading, garbage, writing, garbage, waiting = open.split
+				out.merge({:active_connections => active.gsub(/.*:/,''), :accepted => accepts, :handled => handled, :requests => requests, :reading => reading, :writing => writing, :waiting => waiting})
+		end
+		
+		out.merge({:num_masters => out[:masters].count, :num_workers => out[:workers].count, :summary => [:num_masters, :num_workers, :active_connections]})
 	end
 	
 end
--- lib/city_watch/watchmen/unicorns.rb
+++ lib/city_watch/watchmen/unicorns.rb
@@ -3,13 +3,16 @@ class Unicorns
 	include Watchman
 	
 	def self.data
-		out = PS.data.inject({:masters => [], :workers => []}) do |acc,line|
+		out = PS.data.inject({:masters => [], :workers => [], :master_memory => 0, :worker_memory => 0, :total_memory => 0}) do |acc,line|
 			if line[:command][/^start master/]
 				acc[:masters] << line
+				acc[:master_memory] += line[:rss].to_i
 			end
 			if line[:command][/^start worker/]
 				acc[:workers] << line
+				acc[:worker_memory] += line[:rss].to_i
 			end
+			acc[:total_memory] += line[:rss].to_i
 			acc
 		end
 		out.merge({:num_masters => out[:masters].count, :num_workers => out[:workers].count, :summary => [:num_masters, :num_workers]})
