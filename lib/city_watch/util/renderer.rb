0733c5cf77c72dc5fc61b5aaa35a805a48d74f22
John Bragg <remotezygote@gmail.com>
2013-03-18 13:59:22 -0700

    global template/layout cache

--- lib/city_watch/util/renderer.rb
+++ lib/city_watch/util/renderer.rb
@@ -1,6 +1,13 @@
+require 'erubis'
+
+TemplateCache = {}
+ViewPath = File.expand_path(File.dirname(__FILE__) + "/../../../views")
+Layout = Proc.new {
+	file_path = "#{ViewPath}/layouts/default.html.erb"
+	Erubis::FastEruby.new(File.read(file_path), bufvar: '@output_buffer', filename: file_path)
+}.call
+
 module Renderer
-	
-	require 'erubis'
 	
 	def render(*tpl)
 		layout do
@@ -15,11 +22,7 @@ module Renderer
 	private
 	
 	def layout(&block)
-		@layout ||= Proc.new {
-			file_path = "#{view_path}/layouts/default.html.erb"
-			Erubis::FastEruby.new(File.read(file_path), bufvar: '@output_buffer', filename: file_path)
-		}.call
-		@layout.result(binding)
+		Layout.result(binding)
 	end
 	
 	def template_content(file_path)
@@ -27,24 +30,19 @@ module Renderer
 		File.read(file_path)
 	end
 	
-	def template_key(*keys)
-		keys.inject("") {|a,k| a += k.to_s}
-	end
-
 	def template_path(*tpl)
 		tpl.map {|t| "#{view_path}/#{t}.html.erb" }.detect{|p| File.exists?(p) }
 	end
 	
 	def template(*tpl)
-		return @templates[tpl.join] if @templates && @templates[tpl.join]
+		return TemplateCache[tpl.join] if TemplateCache[tpl.join]
 		file_path = template_path(*tpl)
 		return template_content(file_path) if CityWatch.debug?
-		@templates ||= {}
-		@templates[tpl.join] ||= template_content(file_path)
+		TemplateCache[tpl.join] ||= template_content(file_path)
 	end
 	
 	def view_path
-		File.expand_path(File.dirname(__FILE__) + "/../../../views")
+		ViewPath
 	end
 	
 end
